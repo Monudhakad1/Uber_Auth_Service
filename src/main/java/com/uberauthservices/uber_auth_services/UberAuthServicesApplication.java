52a2b14c129478d4dd59702a56575bef7791d958
Monudhakad1 <monudhakad055@gmail.com>
2025-07-08 22:46:29 +0530

    Application.properties configured

---
+++ src/main/java/com/uberauthservices/uber_auth_services/UberAuthServicesApplication.java
@@ -0,0 +1,13 @@
+package com.uberauthservices.uber_auth_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberAuthServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberAuthServicesApplication.class, args);
+    }
+
+}
